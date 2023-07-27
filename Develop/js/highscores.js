02808b97537d9ce1dfa6a5652faa74c5bdf80c30
marien <mariencastellanosjr@gmail.com>
2023-07-27 19:45:27 -0400

    Refresh so everyone is on the same page

---
+++ Develop/js/highscores.js
@@ -0,0 +1,9 @@
+ var requestUrl = "https://api.imgflip.com/get_memes"
+ fetch(requestUrl)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
+
