b3e3ea39480ce8280d9c2275eec48df6b899ee92
marien <mariencastellanosjr@gmail.com>
2023-07-27 20:07:23 -0400

    test

--- Develop/js/highscores.js
+++ Develop/js/highscores.js
@@ -1,9 +1,12 @@
- var requestUrl = "https://api.imgflip.com/get_memes"
- fetch(requestUrl)
-  .then(function (response) {
-    return response.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+//  var requestUrl = "https://api.imgflip.com/get_memes"
+ 
+//  fetch(requestUrl)
+//   .then(function (response) {
+//     return response.json();
+//   })
+//   .then(function (data) {
+//     console.log(data);
+//   });
+
+console.log("hello");
 
